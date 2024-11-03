eb59f38d1944d32ecffe5b244bef81df69f9e6cd
lanberb <lanberb.2@gmail.com>
2024-11-03 17:52:03 +0900

    fix: globalstyles

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,7 +4,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { ConnectedHomePage } from "./components/pages/Home/ConnectedHomePage";
-import { GlobalStyles } from "./components/styles/GlobalStyles";
+import { GlobalStyles } from "./assets/styles/GlobalStyles";
 
 function main() {
   // biome-ignore lint/style/noNonNullAssertion: <explanation>
